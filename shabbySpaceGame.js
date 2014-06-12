bc2a7ea153842afbe5119b35d0c1c71d4578ce09
Kichiguy <sedarmor@gmail.com>
2014-06-11 20:24:04 -0500

    Basic art assets
    Added placeholder art assets to serve as the player character and
    obstacles. A placeholder background color was also added.

--- shabbySpaceGame.js
+++ shabbySpaceGame.js
@@ -1,7 +1,9 @@
 var game = new Phaser.Game(450, 600, Phaser.AUTO, 'game_div');
 
 function preload() {
-
+	game.stage.backgroundColor = '#000000';
+	game.load.image('ship','ship.png');
+	game.load.image('obstacle','obstacle.png');
 }
 
 function create() {
@@ -9,5 +11,5 @@ function create() {
 }
 
 function update() {
-	
+
 }
